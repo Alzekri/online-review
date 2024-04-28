a756dc832f52e510f63ae9f8fc96b93dad20613c
Alzekri <abdulrahmanalzekri2020@gmail.com>
2024-04-28 19:28:38 +0300

    on

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -7,9 +7,104 @@
                     <img src="@/assets/logo.webp" class="rounded-5" alt="" width="50">
                     <p class="m-2 mx-5 border-bottom">Samar Ali</p>
                     <a class="m-2" href="http://localhost:5173/posts"> visit link</a>
-                    <p class="m-2 ms-5">2022/22/3</p>
                 </div>
-                <h6>Name</h6>
+                <h6>Title</h6>
+                <p>Description</p>
+                <div class="d-flex justify-content-between mt-2">
+                    <div>
+                        <p class="text-center">Befor</p>
+                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
+                    </div>
+                    <div>
+                        <p class="text-center">After</p>
+                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
+                    </div>
+                </div>
+                <div class="comments mt-5">
+                    <div class="comment">
+                        <div class="comment-author">John Smith</div>
+                        <div class="comment-text">Great transformation!</div>
+                    </div>
+                    <div class="comment">
+                        <div class="comment-author">Jane Doe</div>
+                        <div class="comment-text">Love the new look!</div>
+                    </div>
+                    <div class="d-flex "> 
+                        <input type=" text" class="form-control w-75 rounded-5" placeholder="Write Your Comment"/>
+                        <button class="btn  rounded-5 text-light  px-4 ms-3">post</button>
+                    </div>
+                </div>
+            </div><div class="border border-dark rounded-5 m-3 p-3" style="width: 30%;">
+                <div class="d-flex mb-2">
+                    <img src="@/assets/logo.webp" class="rounded-5" alt="" width="50">
+                    <p class="m-2 mx-5 border-bottom">Samar Ali</p>
+                    <a class="m-2" href="http://localhost:5173/posts"> visit link</a>
+                </div>
+                <h6>Title</h6>
+                <p>Description</p>
+                <div class="d-flex justify-content-between mt-2">
+                    <div>
+                        <p class="text-center">Befor</p>
+                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
+                    </div>
+                    <div>
+                        <p class="text-center">After</p>
+                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
+                    </div>
+                </div>
+                <div class="comments mt-5">
+                    <div class="comment">
+                        <div class="comment-author">John Smith</div>
+                        <div class="comment-text">Great transformation!</div>
+                    </div>
+                    <div class="comment">
+                        <div class="comment-author">Jane Doe</div>
+                        <div class="comment-text">Love the new look!</div>
+                    </div>
+                    <div class="d-flex "> 
+                        <input type=" text" class="form-control w-75 rounded-5" placeholder="Write Your Comment"/>
+                        <button class="btn  rounded-5 text-light  px-4 ms-3">post</button>
+                    </div>
+                </div>
+            </div><div class="border border-dark rounded-5 m-3 p-3" style="width: 30%;">
+                <div class="d-flex mb-2">
+                    <img src="@/assets/logo.webp" class="rounded-5" alt="" width="50">
+                    <p class="m-2 mx-5 border-bottom">Samar Ali</p>
+                    <a class="m-2" href="http://localhost:5173/posts"> visit link</a>
+                </div>
+                <h6>Title</h6>
+                <p>Description</p>
+                <div class="d-flex justify-content-between mt-2">
+                    <div>
+                        <p class="text-center">Befor</p>
+                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
+                    </div>
+                    <div>
+                        <p class="text-center">After</p>
+                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
+                    </div>
+                </div>
+                <div class="comments mt-5">
+                    <div class="comment">
+                        <div class="comment-author">John Smith</div>
+                        <div class="comment-text">Great transformation!</div>
+                    </div>
+                    <div class="comment">
+                        <div class="comment-author">Jane Doe</div>
+                        <div class="comment-text">Love the new look!</div>
+                    </div>
+                    <div class="d-flex "> 
+                        <input type=" text" class="form-control w-75 rounded-5" placeholder="Write Your Comment"/>
+                        <button class="btn  rounded-5 text-light  px-4 ms-3">post</button>
+                    </div>
+                </div>
+            </div><div class="border border-dark rounded-5 m-3 p-3" style="width: 30%;">
+                <div class="d-flex mb-2">
+                    <img src="@/assets/logo.webp" class="rounded-5" alt="" width="50">
+                    <p class="m-2 mx-5 border-bottom">Samar Ali</p>
+                    <a class="m-2" href="http://localhost:5173/posts"> visit link</a>
+                </div>
+                <h6>Title</h6>
                 <p>Description</p>
                 <div class="d-flex justify-content-between mt-2">
                     <div>
@@ -37,146 +132,7 @@
                 </div>
             </div>
 
-            <div class="border border-dark rounded-5 m-3 p-3" style="width: 30%;">
-                <div class="d-flex">
-                    <img src="@/assets/logo.webp" class="rounded-5" alt="" width="50">
-                    <p class="m-2 ms-5 border-bottom">Samar Ali</p>
-                </div>
-                <div class="d-flex justify-content-between mt-5">
-                    <div>
-                        <p class="text-center">Befor</p>
-                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
-                    </div>
-                    <div>
-                        <p class="text-center">After</p>
-                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
-                    </div>
-                </div>
-                <div class="comments mt-5">
-                    <div class="comment">
-                        <div class="comment-author">David Johnson</div>
-                        <div class="comment-text">Impressive transformation!</div>
-                    </div>
-                    <div class="comment">
-                        <div class="comment-author">Sarah Thompson</div>
-                        <div class="comment-text">You look amazing!</div>
-                    </div>
-                    <div class="p-3"> <input type=" text" class="form-control rounded-5" placeholder="Write Your Comment">
-                    </div>
-                </div>
-            </div>
-            <div class="border border-dark rounded-5 m-3 p-3" style="width: 30%;">
-                <div class="d-flex">
-                    <img src="@/assets/logo.webp" class="rounded-5" alt="" width="50">
-                    <p class="m-2 ms-5 border-bottom">Samar Ali</p>
-                </div>
-                <div class="d-flex justify-content-between mt-5">
-                    <div>
-                        <p class="text-center">Befor</p>
-                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
-                    </div>
-                    <div>
-                        <p class="text-center">After</p>
-                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
-                    </div>
-                </div>
-                <div class="comments mt-5">
-                    <div class="comment">
-                        <div class="comment-author">David Johnson</div>
-                        <div class="comment-text">Impressive transformation!</div>
-                    </div>
-                    <div class="comment">
-                        <div class="comment-author">Sarah Thompson</div>
-                        <div class="comment-text">You look amazing!</div>
-                    </div>
-                    <div class="p-3"> <input type=" text" class="form-control rounded-5" placeholder="Write Your Comment">
-                    </div>
-                </div>
-            </div>
-            <div class="border border-dark rounded-5 m-3 p-3" style="width: 30%;">
-                <div class="d-flex">
-                    <img src="@/assets/logo.webp" class="rounded-5" alt="" width="50">
-                    <p class="m-2 ms-5 border-bottom">Samar Ali</p>
-                </div>
-                <div class="d-flex justify-content-between mt-5">
-                    <div>
-                        <p class="text-center">Befor</p>
-                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
-                    </div>
-                    <div>
-                        <p class="text-center">After</p>
-                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
-                    </div>
-                </div>
-                <div class="comments mt-5">
-                    <div class="comment">
-                        <div class="comment-author">David Johnson</div>
-                        <div class="comment-text">Impressive transformation!</div>
-                    </div>
-                    <div class="comment">
-                        <div class="comment-author">Sarah Thompson</div>
-                        <div class="comment-text">You look amazing!</div>
-                    </div>
-                    <div class="p-3"> <input type=" text" class="form-control rounded-5" placeholder="Write Your Comment">
-                    </div>
-                </div>
-            </div>
-            <div class="border border-dark rounded-5 m-3 p-3" style="width: 30%;">
-                <div class="d-flex">
-                    <img src="@/assets/logo.webp" class="rounded-5" alt="" width="50">
-                    <p class="m-2 ms-5 border-bottom">Samar Ali</p>
-                </div>
-                <div class="d-flex justify-content-between mt-5">
-                    <div>
-                        <p class="text-center">Befor</p>
-                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
-                    </div>
-                    <div>
-                        <p class="text-center">After</p>
-                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
-                    </div>
-                </div>
-                <div class="comments mt-5">
-                    <div class="comment">
-                        <div class="comment-author">David Johnson</div>
-                        <div class="comment-text">Impressive transformation!</div>
-                    </div>
-                    <div class="comment">
-                        <div class="comment-author">Sarah Thompson</div>
-                        <div class="comment-text">You look amazing!</div>
-                    </div>
-                    <div class="p-3"> <input type=" text" class="form-control rounded-5" placeholder="Write Your Comment">
-                    </div>
-                </div>
-            </div>
-            <div class="border border-dark rounded-5 m-3 p-3" style="width: 30%;">
-                <div class="d-flex">
-                    <img src="@/assets/logo.webp" class="rounded-5" alt="" width="50">
-                    <p class="m-2 ms-5 border-bottom">Samar Ali</p>
-                </div>
-                <div class="d-flex justify-content-between mt-5">
-                    <div>
-                        <p class="text-center">Befor</p>
-                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
-                    </div>
-                    <div>
-                        <p class="text-center">After</p>
-                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
-                    </div>
-                </div>
-                <div class="comments mt-5">
-                    <div class="comment">
-                        <div class="comment-author">David Johnson</div>
-                        <div class="comment-text">Impressive transformation!</div>
-                    </div>
-                    <div class="comment">
-                        <div class="comment-author">Sarah Thompson</div>
-                        <div class="comment-text">You look amazing!</div>
-                    </div>
-                    <div class="p-3"> <input type=" text" class="form-control rounded-5" placeholder="Write Your Comment">
-                    </div>
-                </div>
-            </div>
+      
         </div>
     </div>
 </template>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -5,11 +5,14 @@
         </div>
         <div class="d-flex justify-content-between flex-wrap">
             <div class="border border-dark rounded-5 m-3 p-3" style="width: 30%;">
-                <div class="d-flex">
+                <div class="d-flex mb-2">
                     <img src="@/assets/logo.webp" class="rounded-5" alt="" width="50">
-                    <p class="m-2 ms-5 border-bottom">Reef Lem </p>
+                    <p class="m-2 mx-5 border-bottom">Samar Ali</p>
+                    <a class="m-2" href="http://localhost:5173/posts"> visit link</a>
                 </div>
-                <div class="d-flex justify-content-between mt-5">
+                <h6>Title</h6>
+                <p>Description</p>
+                <div class="d-flex justify-content-between mt-2">
                     <div>
                         <p class="text-center">Befor</p>
                         <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
@@ -28,17 +31,21 @@
                         <div class="comment-author">Jane Doe</div>
                         <div class="comment-text">Love the new look!</div>
                     </div>
-                    <div class="p-3"> <input type=" text" class="form-control rounded-5" placeholder="Write Your Comment">
+                    <div class="d-flex "> 
+                        <input type=" text" class="form-control w-75 rounded-5" placeholder="Write Your Comment"/>
+                        <button class="btn  rounded-5 text-light  px-4 ms-3">post</button>
                     </div>
                 </div>
             </div>
-
             <div class="border border-dark rounded-5 m-3 p-3" style="width: 30%;">
-                <div class="d-flex">
+                <div class="d-flex mb-2">
                     <img src="@/assets/logo.webp" class="rounded-5" alt="" width="50">
-                    <p class="m-2 ms-5 border-bottom">Reef Lem </p>
+                    <p class="m-2 mx-5 border-bottom">Samar Ali</p>
+                    <a class="m-2" href="http://localhost:5173/posts"> visit link</a>
                 </div>
-                <div class="d-flex justify-content-between mt-5">
+                <h6>Title</h6>
+                <p>Description</p>
+                <div class="d-flex justify-content-between mt-2">
                     <div>
                         <p class="text-center">Befor</p>
                         <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
@@ -50,23 +57,28 @@
                 </div>
                 <div class="comments mt-5">
                     <div class="comment">
-                        <div class="comment-author">David Johnson</div>
-                        <div class="comment-text">Impressive transformation!</div>
+                        <div class="comment-author">John Smith</div>
+                        <div class="comment-text">Great transformation!</div>
                     </div>
                     <div class="comment">
-                        <div class="comment-author">Sarah Thompson</div>
-                        <div class="comment-text">You look amazing!</div>
+                        <div class="comment-author">Jane Doe</div>
+                        <div class="comment-text">Love the new look!</div>
                     </div>
-                    <div class="p-3"> <input type=" text" class="form-control rounded-5" placeholder="Write Your Comment">
+                    <div class="d-flex "> 
+                        <input type=" text" class="form-control w-75 rounded-5" placeholder="Write Your Comment"/>
+                        <button class="btn  rounded-5 text-light  px-4 ms-3">post</button>
                     </div>
                 </div>
             </div>
             <div class="border border-dark rounded-5 m-3 p-3" style="width: 30%;">
-                <div class="d-flex">
+                <div class="d-flex mb-2">
                     <img src="@/assets/logo.webp" class="rounded-5" alt="" width="50">
-                    <p class="m-2 ms-5 border-bottom">Reef Lem </p>
+                    <p class="m-2 mx-5 border-bottom">Samar Ali</p>
+                    <a class="m-2" href="http://localhost:5173/posts"> visit link</a>
                 </div>
-                <div class="d-flex justify-content-between mt-5">
+                <h6>Title</h6>
+                <p>Description</p>
+                <div class="d-flex justify-content-between mt-2">
                     <div>
                         <p class="text-center">Befor</p>
                         <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
@@ -78,98 +90,16 @@
                 </div>
                 <div class="comments mt-5">
                     <div class="comment">
-                        <div class="comment-author">David Johnson</div>
-                        <div class="comment-text">Impressive transformation!</div>
+                        <div class="comment-author">John Smith</div>
+                        <div class="comment-text">Great transformation!</div>
                     </div>
                     <div class="comment">
-                        <div class="comment-author">Sarah Thompson</div>
-                        <div class="comment-text">You look amazing!</div>
+                        <div class="comment-author">Jane Doe</div>
+                        <div class="comment-text">Love the new look!</div>
                     </div>
-                    <div class="p-3"> <input type=" text" class="form-control rounded-5" placeholder="Write Your Comment">
-                    </div>
-                </div>
-            </div>
-            <div class="border border-dark rounded-5 m-3 p-3" style="width: 30%;">
-                <div class="d-flex">
-                    <img src="@/assets/logo.webp" class="rounded-5" alt="" width="50">
-                    <p class="m-2 ms-5 border-bottom">Reef Lem </p>
-                </div>
-                <div class="d-flex justify-content-between mt-5">
-                    <div>
-                        <p class="text-center">Befor</p>
-                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
-                    </div>
-                    <div>
-                        <p class="text-center">After</p>
-                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
-                    </div>
-                </div>
-                <div class="comments mt-5">
-                    <div class="comment">
-                        <div class="comment-author">David Johnson</div>
-                        <div class="comment-text">Impressive transformation!</div>
-                    </div>
-                    <div class="comment">
-                        <div class="comment-author">Sarah Thompson</div>
-                        <div class="comment-text">You look amazing!</div>
-                    </div>
-                    <div class="p-3"> <input type=" text" class="form-control rounded-5" placeholder="Write Your Comment">
-                    </div>
-                </div>
-            </div>
-            <div class="border border-dark rounded-5 m-3 p-3" style="width: 30%;">
-                <div class="d-flex">
-                    <img src="@/assets/logo.webp" class="rounded-5" alt="" width="50">
-                    <p class="m-2 ms-5 border-bottom">Reef Lem </p>
-                </div>
-                <div class="d-flex justify-content-between mt-5">
-                    <div>
-                        <p class="text-center">Befor</p>
-                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
-                    </div>
-                    <div>
-                        <p class="text-center">After</p>
-                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
-                    </div>
-                </div>
-                <div class="comments mt-5">
-                    <div class="comment">
-                        <div class="comment-author">David Johnson</div>
-                        <div class="comment-text">Impressive transformation!</div>
-                    </div>
-                    <div class="comment">
-                        <div class="comment-author">Sarah Thompson</div>
-                        <div class="comment-text">You look amazing!</div>
-                    </div>
-                    <div class="p-3"> <input type=" text" class="form-control rounded-5" placeholder="Write Your Comment">
-                    </div>
-                </div>
-            </div>
-            <div class="border border-dark rounded-5 m-3 p-3" style="width: 30%;">
-                <div class="d-flex">
-                    <img src="@/assets/logo.webp" class="rounded-5" alt="" width="50">
-                    <p class="m-2 ms-5 border-bottom">Reef Lem </p>
-                </div>
-                <div class="d-flex justify-content-between mt-5">
-                    <div>
-                        <p class="text-center">Befor</p>
-                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
-                    </div>
-                    <div>
-                        <p class="text-center">After</p>
-                        <img src="@/assets/logo.webp" class="rounded-5" alt="" width="130">
-                    </div>
-                </div>
-                <div class="comments mt-5">
-                    <div class="comment">
-                        <div class="comment-author">David Johnson</div>
-                        <div class="comment-text">Impressive transformation!</div>
-                    </div>
-                    <div class="comment">
-                        <div class="comment-author">Sarah Thompson</div>
-                        <div class="comment-text">You look amazing!</div>
-                    </div>
-                    <div class="p-3"> <input type=" text" class="form-control rounded-5" placeholder="Write Your Comment">
+                    <div class="d-flex "> 
+                        <input type=" text" class="form-control w-75 rounded-5" placeholder="Write Your Comment"/>
+                        <button class="btn  rounded-5 text-light  px-4 ms-3">post</button>
                     </div>
                 </div>
             </div>
